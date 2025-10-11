465695087e2a69a6a19e88368887442686dbcfc2
Paulo-Thomaz-Filho <thomazfilhop@gmail.com>
2025-10-11 09:52:11 -0300

    atualização de acesso as paginas de admin

--- app/admin/verifica_login.php
+++ app/admin/verifica_login.php
@@ -1,6 +1,4 @@
 <?php
-session_start();
-
 // Verifica se a variável de sessão 'user_logged_in' não existe ou não é verdadeira.
 if (!isset($_SESSION['user_logged_in']) || $_SESSION['user_logged_in'] !== true) {
     header('Location: login');
